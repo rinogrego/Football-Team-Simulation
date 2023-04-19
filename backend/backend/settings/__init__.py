6f41f2f90ed67196582155ca4b76566f4dcc2bd3
rinogrego <rino.grego@gmail.com>
2023-04-20 00:23:06 +0700

    configure dev & prod environment settings

---
+++ backend/backend/settings/__init__.py
@@ -0,0 +1,10 @@
+from .base import *
+
+
+DEPLOYMENT = os.getenv("DEPLOYMENT_MODE")
+
+if DEPLOYMENT == "dev":
+    from .dev import *
+    
+elif DEPLOYMENT == "prod":
+    from .prod import *
---
+++ backend/backend/settings/prod.py
@@ -0,0 +1,17 @@
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "football-team-simulation-production.up.railway.app"]
+
+# os package will be inherited from base.py
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
+}
