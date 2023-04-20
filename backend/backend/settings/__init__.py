1a99c2fad99da111517e7dea4539a4f418c37dcf
rinogrego <rino.grego@gmail.com>
2023-04-21 00:09:22 +0700

    trying DEBUG=False

--- backend/backend/settings/__init__.py
+++ backend/backend/settings/__init__.py
@@ -10,4 +10,4 @@ elif DEPLOYMENT == "prod":
     from .prod import *
     
 # DEBUG = True because otherwise railway interal server error (500), but somehow API request via script (test_request_api.py) still works
-DEBUG = True
+# DEBUG = True
--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -120,7 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
